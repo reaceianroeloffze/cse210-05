692ecb0375b0ce782ed4e1f1ab43d7732450d89c
= <=>
2022-06-17 20:55:27 +0200

    Updating movements of cyclists

--- cycle/game/scripting/control_actors_action.py
+++ cycle/game/scripting/control_actors_action.py
@@ -31,19 +31,19 @@ class ControlActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         # left
-        if self._keyboard_service.is_key_down('a'):
+        if self._keyboard_service.is_key_down('a') or self._keyboard_service.is_key_down('j'):
             self._direction = Point(-constants.CELL_SIZE, 0)
         
         # right
-        if self._keyboard_service.is_key_down('d'):
+        if self._keyboard_service.is_key_down('d') or self._keyboard_service.is_key_down('l'):
             self._direction = Point(constants.CELL_SIZE, 0)
         
         # up
-        if self._keyboard_service.is_key_down('w'):
+        if self._keyboard_service.is_key_down('w') or self._keyboard_service.is_key_down('i'):
             self._direction = Point(0, -constants.CELL_SIZE)
         
         # down
-        if self._keyboard_service.is_key_down('s'):
+        if self._keyboard_service.is_key_down('s') or self._keyboard_service.is_key_down('k'):
             self._direction = Point(0, constants.CELL_SIZE)
         
         cyclist_1 = cast.get_first_actor("snakes")
