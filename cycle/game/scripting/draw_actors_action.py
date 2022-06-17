e2222a0e5fece98fd0aeb75146f62058091b154f
= <=>
2022-06-17 21:47:44 +0200

    Updated actors' actions

--- cycle/game/scripting/draw_actors_action.py
+++ cycle/game/scripting/draw_actors_action.py
@@ -27,14 +27,17 @@ class DrawActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
-        snake = cast.get_first_actor("snakes")
-        segments = snake.get_segments()
+        # food = cast.get_first_actor("foods")
+        cyclist_1 = cast.get_first_actor("cyclist_1")
+        cyclist_2 = cast.get_first_actor("cyclist_2")
+        segments1 = cyclist_1.get_segments()
+        segments2 = cyclist_2.get_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
-        self._video_service.draw_actors(segments)
+        # self._video_service.draw_actor(food)
+        self._video_service.draw_actors(segments1)
+        self._video_service.draw_actors(segments2)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
